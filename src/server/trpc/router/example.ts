e96cbf8bc7b68190286fcb2e517043bad81ccb1b
datisekai <84700751+datisekai@users.noreply.github.com>
2022-08-16 21:02:53 +0700

    first commit

---
+++ src/server/trpc/router/example.ts
@@ -0,0 +1,16 @@
+import { t } from "../utils";
+import { z } from "zod";
+
+export const exampleRouter = t.router({
+  hello: t.procedure
+    .input(z.object({ text: z.string().nullish() }).nullish())
+    .query(({ input }) => {
+      console.log(input);
+      return {
+        greeting: `Hello ${input?.text ?? "world"}`,
+      };
+    }),
+  getAll: t.procedure.query(({ ctx }) => {
+    return ctx.prisma.example.findMany();
+  }),
+});
